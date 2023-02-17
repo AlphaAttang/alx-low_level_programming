a4c87f6a3190153f463db413de8c5c0cf1361b3a
Alpha Attang <alphaa.attang@gmail.com>
2023-02-17 14:17:55 -0800

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints alphabets in lowercase
+ * Return: Always 0
+ */
+
+int main()
+{
+
+	char la;
+
+	for(la = 'a'; la <= 'z'; la++)
+	{
+		putchar(la);
+	}
+	putchar('\n');
+	return (0);
+}
