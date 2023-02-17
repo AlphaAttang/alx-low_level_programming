b7e2ac9cf6bd4c9b4edd12fdd8d4efb0152b4fdb
Alpha Attang <alphaa.attang@gmail.com>
2023-02-17 15:32:20 -0800

    print alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - print all letters except q and e
+ * Return: Always 0
+ */
+
+int main(void)
+{
+
+	char letters;
+
+	for (letters = 'a'; latters <= 'z'; letters++)
+	{
+		if !((letters = 'q') && (letters = 'e'))
+		{
+			putchar(letters);
+		}
+	}
+	putchar('\n');
+	return(0);
+}
