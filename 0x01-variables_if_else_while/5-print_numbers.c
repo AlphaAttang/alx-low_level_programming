e47833eb991daf949bf07ddebd0f0d180292cf7b
Alpha Attang <alphaa.attang@gmail.com>
2023-02-17 16:01:26 -0800

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - print single digits of base 10
+ * Return: Always 0
+ */
+
+int main(void)
+{
+
+	int num;
+
+	for (num = 0; num < 10; num++)
+		printf(num);
+
+	printf('\n');
+	return (0);
+}
