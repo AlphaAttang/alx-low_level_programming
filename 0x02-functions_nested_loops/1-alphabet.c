352fea9f01947f4485eac6f66e42e001966226cf
AlphaAttang <108028892+AlphaAttang@users.noreply.github.com>
2023-02-22 00:24:12 +0100

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,9 +4,10 @@
  * print_alphabet - prints the alphabet in lowercase
  */
 
-void print_alphabet(void);
+void print_alphabet(void)
 {
 	char alphabet;
+	
 	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 		_putchar(alphabet);
 
