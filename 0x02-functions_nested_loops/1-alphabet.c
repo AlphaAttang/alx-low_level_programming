b0ae9dd3bcb332d4d24c4de82faaec6bbb92b932
Alpha Attang <alphaa.attang@gmail.com>
2023-02-21 15:12:19 -0800

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - prints the alphabet in lowercase
+ * Return: Always 0
+ */
+
+int main()
+{
+	char alphabet;
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+		_putchar(alphabet);
+	_putchar('\n');
+	return (0);
+}
