d24929122a5a802b40bc98f4506dc917e2b41fc4
Alpha Attang <alphaa.attang@gmail.com>
2023-02-21 18:28:15 -0800

    absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "math.h"
+
+/**
+ * _abs - computes the absolute value of an integer
+ * @a: integer to be inputed
+ * Return: The absolute value of the integer
+ */
+
+int _abs(int)
+{
+	int a;
+	if (a >= 0)
+		return (a);
+	else
+		return (-a);
+}
