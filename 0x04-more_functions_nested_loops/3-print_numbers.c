309fa000024c6c5b7bea10ead35250133d59f7c8
Alpha Attang <alphaa.attang@gmail.com>
2023-02-23 04:41:30 -0800

    push

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,16 +2,15 @@
 
 /**
  * print_numbers - prints the numbers from 0 - 9
- * Return: void
  */
 
 void print_numbers(void)
 {
-	char c;
+	int num;
 
-	for (c >= '0'; c <= '9'; c++)
+	for (num >= 0; num <= 9; num++)
 	{
-		_putchar(c);
+		_putchar(num);
 	}
 	_putchar('\n');
 }
