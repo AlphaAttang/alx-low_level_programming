924a3071d01264d193ef7604e5626a8a6990166a
Alpha Attang <alphaa.attang@gmail.com>
2023-02-24 01:44:50 -0800

    print square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -14,11 +14,10 @@ void print_square(int size)
 		for (height = 0; height < size; height++)
 		{
 			for (width = 0; width < size; width++)
+			{
 				_putchar('#');
-
-			if (height == size - 1)
-				continue;
-			_putchar('\n');
+				_putchar('\n');
+			}
 		}
 	}
 
