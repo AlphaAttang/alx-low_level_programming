1a237ef4753b544166789e66084c22b61ed2bd80
AlphaAttang <108028892+AlphaAttang@users.noreply.github.com>
2023-02-24 18:58:52 +0100

    Update 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "main.h"
 
 /**
  * main - Prints the numbers from 1-100, but for multiples of three,
@@ -25,7 +26,7 @@ int main(void)
 
 		else
 			printf("%d ", num);
-		
+
 		printf(" ");
 	}
 
