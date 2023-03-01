cf0d0c8638662507e01ca20dbf67c77ef00b316d
AlphaAttang <108028892+AlphaAttang@users.noreply.github.com>
2023-03-01 23:24:37 +0100

    Update 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * void reset_to_98 - a function that takes a pointer to an int as parameter 
- * 		      and updates the value it points to 98
+ * reset_to_98 - a function that takes a pointer to an int as parameter 
+ * 		 and updates the value it points to 98
  * @n: pointer
  */
 
